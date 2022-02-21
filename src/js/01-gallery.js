4f9a1e64e73129c0d0e5bc148c267cc2b3e545dd
Evgenij Sas <e.vasilevich35@gmail.com>
2022-02-21 18:01:21 +0300

    update files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryListRef = document.querySelector('.gallery');
+
+const galleryItem = galleryItems.map(({ preview, original, description }) => {
+  return `<li>
+   <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt=${description}/>
+    </a>
+  </li>`;
+});
+
+galleryListRef.insertAdjacentHTML('beforeend', galleryItem.join(''));
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
